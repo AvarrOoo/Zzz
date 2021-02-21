bac84fba6d90aebe3bdb37c114e5a6f69f1e0ce7
AvarrOoo <79396646+AvarrOoo@users.noreply.github.com>
2021-02-21 11:11:27 +0300

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,9 @@
+[LIST=1]
+[*] apt update && apt upgrade -y
+[*] apt install git -y
+[*] git clone https://github.com/sqlmapproject/sqlmap
+[*] cd sqlmap
+[*]apt install python -y
+[*]chmod 777 sqlmap.py
+[*]python sqlmap.py
+[/LIST]
